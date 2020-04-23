dfcfd501cea1c6e824e097e2c369600bd19facf5
David O'Donovan <d@davidodonovan.com>
2020-04-23 18:03:40 +1000

    add note about default svg width/height dimensions

--- src/01SVGBasicStyling/index.js
+++ src/01SVGBasicStyling/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { ReactComponent as Smiley } from './smiley.svg';
-import { SVGWrapper } from './style';
+import { Grid, SVGWrapper } from './style';
+import { Link } from 'react-router-dom';
+
 
 const BasicStyling=()=>{
   return (
     <div>
-      <SVGWrapper>
-        <Smiley/>
-      </SVGWrapper>
+      <Link to="/" style={{color: "blue"}}>back</Link>
+      <hr/>
+      <div style={{position: "relative"}}>
+        <SVGWrapper>
+          <Smiley/>
+        </SVGWrapper>
+        <div>
+          Note if no width or height is specified, an svg defaults to a width of 300px and height of 150px.
+        </div>
+      </div>
     </div>
   );
 };
--- src/01SVGBasicStyling/style.js
+++ src/01SVGBasicStyling/style.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
+export const Grid=styled.div`
+  border: 1px solid red;
+`;
+
 export const SVGWrapper=styled.div`
   & svg {
     fill: yellow;
     stroke: black;
+    background: red;
     stroke-width: 8px;
     stroke-linecap: round;
     stroke-linejoin: round;
