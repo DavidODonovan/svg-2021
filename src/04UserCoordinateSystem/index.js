d289c2ee77fa2a01c779fa1e310615b76ff20d9c
David O'Donovan <d@davidodonovan.com>
2020-04-24 17:15:56 +1000

    add some more analogies to explanatory text

--- src/04UserCoordinateSystem/index.js
+++ src/04UserCoordinateSystem/index.js
@@ -11,16 +11,16 @@ const BasicStyling=()=>{
       <hr/>
       <strong>04 USER coordinate system</strong>
       <hr/>
-        <p>The <strong>user coordinate system </strong> is the coordinate system on the SVG element, which you might think of as the <em>canvas</em> or  the svg-world where distance stretches infinitely to every horizon. I think of this coordinate system as kind of like if I was standing on this svg landscape, that I could draw in chalk on the ground a metre by metre grid, radiating outwards forever.
-        </p>
-        <br/>
-        <p>
-         This <strong>user coordinate system </strong>is initially identical to the <strong>viewport coordinate system</strong>, in other words it is a one to one mapping. We can change the way the user coord system maps to the viewport coord system by setting an attribute called the <strong>viewBox</strong>.
-        </p>
-        <br/>
-
+      <p>The <strong>user coordinate system </strong> is the coordinate system <strong><em>on the SVG element,</em></strong> which you might think of as the <em>canvas</em> or  the svg-world where distance stretches infinitely to every horizon. So you might think of this as 'real-world' coordinates in your imaginary world. I think of this coordinate system as kind of like if I was standing on this svg landscape, that I could draw in chalk on the ground a metre by metre grid, radiating outwards forever.
+      </p>
+      <br/>
+      <p>
+       This <strong>user coordinate system </strong>is initially identical to the <strong>viewport coordinate system</strong>, in other words it is a one to one mapping. We can change the way the user-coord-system maps to the viewport-coord-system by setting an attribute called the <strong>viewBox</strong>. We will look at the viewBox attribute in the next section.
+      </p>
+      <br/>
 
       <img src={sketch1} width="500px"/>
+
       <br/>
     </div>
   );
--- src/05ViewboxAttribute/index.js
+++ src/05ViewboxAttribute/index.js
@@ -24,28 +24,29 @@ const BasicStyling=()=>{
       </SVGWrapper>
       <br/>
 
-      <p>
-       Note how the user-coordinate-system and viewport coordinate system are identical.
-       This is the default; we did not specify <strong>our own coordinate system </strong>by setting up a <strong>viewBox </strong>attribute.
+       <p>
+         When we created a viewport coordinate system by setting the width and height to 100px, the browser created a default user-coordinate-system that matches it exactly.
+         For this reason all the positioning and drawing might appear to have been done relative to the viewport-coord-system. However in actual fact the drawing is done by applying lines and planes to the svg landscape using the user-coord-system, and it has been set up by default to be a one-to-one mapping of the viewport-coord-system.
+         So right now the user-coordinate-system and viewport-coordinate-system are identical.
+       </p>
+       <br/>
+       <p>If we want to <strong><em>scale</em></strong> our vector graphic, we are going to need to use the <strong>viewBox </strong> attribute.
        </p>
        <br/>
 
        <p>
-         When we created a viewport coordinate system by setting the width and height to 100px, the browser created a default user coordinate system that matches it exactly.
-         For this reason all the positioning and drawing seems to be done relative to the viewport-coord-system. However in actual fact the drawing is done by applying lines and planes to the svg landscape using the user-coord-system, and it has been set up by default to be a one-to-one mapping of the viewport-coord-system.
+          This is where you might begin to imagine the svg specification as being like a camera; it has a viewfinder which we look through; the viewport. Using this 'window' onto the svg-world we are initially using a 'lens' that doesn't magnify or contract the image, but instead gives us a view which is kind of like we have placed our large camera flat to the 'ground' of the svg-world, so the mapping is one-to-one. I imagine it as kind of like the camera is a large house-window that we have laid on the 'ground' of the svg-world.
        </p>
        <br/>
-
-       <p>
-          This is where I begin to imagine the svg spec is like a camera; it has a viewfinder which we look through; the viewport. Using this 'window' onto the svg world we are initially using a 'lens' that doesn't magnify or contract the image, but instead gives us a view which is kind of like we have placed our large camera flat to the 'ground' of the svg-world, so the mapping is one-to-one. I imagine it as kind of like the camera is a large house-window that we have laid on the 'ground' of the svg-world.
-       </p>
 
        <p>
          We can think of the user coordinate setting as kind of like changing lenses on a camera; we get a different field of view and a different magnification or 'zoom'. We can make 'features' on the svg landscape look closer or further away, larger or smaller, and located in different places in our 'photo' just by changing lenses and adjusting the direction our lens is pointing. With our special svg-camera we can also change the aspect ratio of the shot, so it's a pretty cool camera.
        </p>
 
       <hr/>
-      When we use the viewBox attribute we can  <strong>customise our user coordinate system</strong>.
+      So we use the <strong>viewBox </strong>attribute to <strong>customise our user-coordinate-system</strong>, which means essentially the viewBox attribute is how we <strong><em>scale</em></strong> our 'photo' of the svg-world. So the viewBox attribute is a <em>scaling mechanism.</em>
+      <br/>
+      <br/>
       <SVGWrapper width={100} height={100}>
         <svg xmlns="http://www.w3.org/2000/svg">
           <path d="M50,10 A40,40,1,1,1,50,90 A40,40,1,1,1,50,10 M30,40 Q36,35,42,40 M58,40 Q64,35,70,40 M30,60 Q50,75,70,60 Q50,75,30,60"></path>
