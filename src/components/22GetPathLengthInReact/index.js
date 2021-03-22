a031ceac0b9cb47444f3ef40edcb653ae538a0c5
Davido <d@davidodonovan.com>
2021-03-22 15:05:36 +1100

    grab path lengths and save them in component level state

--- src/components/22GetPathLengthInReact/index.js
+++ src/components/22GetPathLengthInReact/index.js
@@ -1,21 +1,47 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { MyLogo } from './style';
 
 const GetPathLengthInReact=()=>{
   const domNode = useRef(null);
+  const [pathLengths, setPathLengths]=useState({});
+  const [objectToText, setObjectToText]=useState([]);
 
-  // use a nested for-loop to iterate over children
-  // try a recursive approach
   useEffect(()=>{
+    let newObj = {};
     let domNodeCurrent = domNode.current;
+    let layers = domNodeCurrent.children;
+    for(let i=0; i<layers.length; i++){
+      let layer = layers[i]
+      let layerName = layer.id;
+      let pathLength = layer.children[0].getTotalLength();
+      newObj[layerName] = Math.floor(pathLength);
+    };
+    setPathLengths(newObj);
+
+    console.log(domNodeCurrent.children[0])
+    console.log(domNodeCurrent.children[1])
+    console.log(domNodeCurrent.children[2].children[0])
     console.log(domNodeCurrent.children[0].id);
-    console.log(domNodeCurrent.children[0]["id"]);
+    console.log(domNodeCurrent.children[0].id);
     console.log(domNodeCurrent.children[0].children[0].getTotalLength());
-    domNodeCurrent.children[0].children[0].style.setProperty("fill", "blue", "important")//<--why isn't this working?..
+    domNodeCurrent.children[0].children[0].style.setProperty("fill", "blue", "important")
 
   }, [domNode]);
+
+  useEffect(()=>{
+    let keys = Object.keys(pathLengths);
+    let newArr = keys.map((key)=>{
+      return (<li>{key} : {pathLengths[key]}</li>)
+    });
+    setObjectToText(newArr);
+  },[pathLengths]);
+
   return (
     <div>
+      <div>
+        <strong>Path lengths:</strong>
+      </div>
+      <div>{objectToText}</div>
       <MyLogo ref={domNode}/>
     </div>
   );
